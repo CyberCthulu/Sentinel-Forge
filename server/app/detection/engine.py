66c603bb6c4bb9f23b049e4aaa0ec074860a0218
vinal <vinchau01@gmail.com>
2026-04-22 20:44:30 -0700

    added placeholder engine.py

---
+++ server/app/detection/engine.py
@@ -0,0 +1,7 @@
+def detect(events):
+    return {
+        "failed_logins": detect_failed_logins(events),
+        "suspicious_login": detect_suspicious_login(events),
+        "lateral_movement": detect_lateral(events),
+        "drone_activity": detect_drone(events),
+    }
